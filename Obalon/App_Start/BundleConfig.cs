3121fd23113ee55ce3d9abf677b344a4dd0447a6
Manu_zd <emanuel.moculescu@intelactsoft.com>
2016-10-18 17:20:06 +0300

    Minor Initial Functionality

--- Obalon/App_Start/BundleConfig.cs
+++ Obalon/App_Start/BundleConfig.cs
@@ -8,6 +8,7 @@ namespace Obalon
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
+
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
 
@@ -26,6 +27,10 @@ namespace Obalon
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/common").Include(
+                        "~/Scripts/common/global.js"));
+
         }
     }
 }
